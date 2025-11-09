c5db0364b6dd5d522c481f6b82e5c0fd0b7d6a14
ronlevi1 <ronlevi42@gmail.com>
2025-11-09 22:28:10 +0200

    Change Theme

---
+++ 01_HW/audioList.js
@@ -0,0 +1,7 @@
+const skins = ["./SKINS/basic.css", "./SKINS/dark.css", "./SKINS/modern.css"];
+
+let currentSkin = 0;
+function changeTheme(){
+    currentSkin = (currentSkin + 1) % skins.length;
+    document.querySelector("link[rel='stylesheet']").href = skins[currentSkin]
+}
